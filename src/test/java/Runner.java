e7ed28db7929ab42e9bfbaad3db9d4ba3030756d
pascal leclercq <pascal.leclercq@gmail.com>
2013-06-02 11:14:18 +0200

    OPEN - task 12: how we can install it ? https://github.com/angelozerr/mongo-mapreduce-webbrowser/issues/issue/12

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -6,18 +6,50 @@ import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Runner {
 
+	private static final int DUMMY_PORT = 12345;
+	private static final String ROOT_CONTEXT_PATH = "/";
+
 	public static void main(String[] args) throws Exception {
-		Server server = new Server(12345);
+		int port =getPort(args);
+		String contextpath=getContextpath(args);
+		Server server = new Server(port);
 		ProtectionDomain domain = Runner.class.getProtectionDomain();
 		URL location = domain.getCodeSource().getLocation();
 		WebAppContext webapp = new WebAppContext();
-		webapp.setContextPath("/");
+		webapp.setContextPath(contextpath);
 		
 		webapp.setWar(location.toExternalForm());
 		server.setHandler(webapp);
 		server.start();
 		server.join();
 
+	}
+
+	private static String getContextpath(String[] args) {
+		String  contextpath=ROOT_CONTEXT_PATH;
+		for (int i = 0; i < args.length; i++) {
+			if("-contextpath".equals(args[i])){
+				contextpath=ROOT_CONTEXT_PATH+args[i+1];
+				break;
+			}
+		}
+		
+		return contextpath;
+	}
+
+	private static int getPort(String[] args) {
+		int port = DUMMY_PORT;
+		for (int i = 0; i < args.length; i++) {
+			if("-port".equals(args[i])){
+				try {
+					port=Integer.parseInt(args[i+1]);
+				} catch (NumberFormatException e) {
+					System.err.println(args[i+1] +" is not an integer");
+				}
+				break;
+			}
+		}
+		return port;
 	}
 
 }
