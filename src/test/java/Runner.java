dc4fe8909eccdcb904226dfbaf381668125828d3
pascal leclercq <pascal.leclercq@gmail.com>
2013-06-09 22:51:56 +0200

    CLOSED - task 12: how we can install it ? https://github.com/angelozerr/mongo-mapreduce-webbrowser/issues/issue/12
    add 
    -data to customize working directory

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.net.URL;
 import java.security.ProtectionDomain;
 
@@ -8,7 +9,7 @@ public class Runner {
 
 	private static final int DUMMY_PORT = 12345;
 	private static final String ROOT_CONTEXT_PATH = "/";
-
+	private static final String DATA_DIR = "data";
 	public static void main(String[] args) throws Exception {
 		int port =getPort(args);
 		String contextpath=getContextpath(args);
@@ -17,7 +18,7 @@ public class Runner {
 		URL location = domain.getCodeSource().getLocation();
 		WebAppContext webapp = new WebAppContext();
 		webapp.setContextPath(contextpath);
-		
+		webapp.setTempDirectory(new File(getData(args)));
 		webapp.setWar(location.toExternalForm());
 		server.setHandler(webapp);
 		server.start();
@@ -37,6 +38,17 @@ public class Runner {
 		return contextpath;
 	}
 
+	private static String getData(String[] args) {
+		String  contextpath=DATA_DIR;
+		for (int i = 0; i < args.length; i++) {
+			if("-data".equals(args[i])){
+				contextpath=args[i+1];
+				break;
+			}
+		}
+		
+		return contextpath;
+	}
 	private static int getPort(String[] args) {
 		int port = DUMMY_PORT;
 		for (int i = 0; i < args.length; i++) {
