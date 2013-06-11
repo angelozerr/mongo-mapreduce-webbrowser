e39ab81c56b120e8124cdc8dd4af61f8afd33612
angelo.zerr@gmail.com <angelo.zerr@gmail.com>
2013-06-12 00:37:12 +0200

    Use last version of Tern.

--- src/main/webapp/resources/mapreduces/default/mr_logs.js
+++ src/main/webapp/resources/mapreduces/default/mr_logs.js
@@ -19,4 +19,77 @@ ${namespace}.reduceFunc=function (key, values) {
 		count : count
 	};
 }
-${namespace}.document=[{"_id":{"$oid":"50d8fdedadd222278ba9090f"},"date":{"$date":"2012-12-25T01:14:21.312Z"},"url":"http;//iiiiiiiiiii"},{"_id":{"$oid":"50d8fdedadd222278ba90910"},"date":{"$date":"2012-12-25T01:14:21.312Z"},"url":"http;//hhhhhhhhh"},{"_id":{"$oid":"50d8fdedadd222278ba90911"},"date":{"$date":"2012-12-25T01:14:21.312Z"},"url":"http;//hhhhhhhhh"},{"_id":{"$oid":"50d8fdedadd222278ba90912"},"date":{"$date":"2012-12-25T01:14:21.312Z"},"url":"http;//hhhhhhhhh"},{"_id":{"$oid":"50d8fdedadd222278ba90913"},"date":{"$date":"2012-12-25T01:14:21.312Z"},"url":"http;//hhhhhhhhh"},{"_id":{"$oid":"50d8fdedadd222278ba90914"},"date":{"$date":"2012-12-25T01:14:21.312Z"},"url":"http;//hhhhhhhhh"},{"_id":{"$oid":"50d8fdedadd222278ba90915"},"date":{"$date":"2012-12-25T01:14:21.312Z"},"url":"http;//hhhhhhhhh"},{"_id":{"$oid":"50d8fdedadd222278ba90916"},"date":{"$date":"2012-12-25T01:14:21.312Z"},"url":"http;//hhhhhhhhh"}]
+${namespace}.document=[
+ {
+  "_id": {
+   "$oid": "50d8fdedadd222278ba9090f"
+  },
+  "date": {
+   "$date": "2012-12-25T01:14:21.312Z"
+  },
+  "url": "http;//iiiiiiiiiii"
+ },
+ {
+  "_id": {
+   "$oid": "50d8fdedadd222278ba90910"
+  },
+  "date": {
+   "$date": "2012-12-25T01:14:21.312Z"
+  },
+  "url": "http;//hhhhhhhhh"
+ },
+ {
+  "_id": {
+   "$oid": "50d8fdedadd222278ba90911"
+  },
+  "date": {
+   "$date": "2012-12-25T01:14:21.312Z"
+  },
+  "url": "http;//hhhhhhhhh"
+ },
+ {
+  "_id": {
+   "$oid": "50d8fdedadd222278ba90912"
+  },
+  "date": {
+   "$date": "2012-12-25T01:14:21.312Z"
+  },
+  "url": "http;//hhhhhhhhh"
+ },
+ {
+  "_id": {
+   "$oid": "50d8fdedadd222278ba90913"
+  },
+  "date": {
+   "$date": "2012-12-25T01:14:21.312Z"
+  },
+  "url": "http;//hhhhhhhhh"
+ },
+ {
+  "_id": {
+   "$oid": "50d8fdedadd222278ba90914"
+  },
+  "date": {
+   "$date": "2012-12-25T01:14:21.312Z"
+  },
+  "url": "http;//hhhhhhhhh"
+ },
+ {
+  "_id": {
+   "$oid": "50d8fdedadd222278ba90915"
+  },
+  "date": {
+   "$date": "2012-12-25T01:14:21.312Z"
+  },
+  "url": "http;//hhhhhhhhh"
+ },
+ {
+  "_id": {
+   "$oid": "50d8fdedadd222278ba90916"
+  },
+  "date": {
+   "$date": "2012-12-25T01:14:21.312Z"
+  },
+  "url": "http;//hhhhhhhhh"
+ }
+]
