9d148b0a8a36d13bc89af1c161ba0cb742825aa3
pascal leclercq <pascal.leclercq@gmail.com>
2013-06-20 09:50:38 +0200

    OPEN - task 12: how we can install it ? https://github.com/angelozerr/mongo-mapreduce-webbrowser/issues/issue/12
    1. renamme artifactId to mmw
    2. unzip into webapp folder

--- src/test/java/bin/StartServer.java
+++ src/test/java/bin/StartServer.java
@@ -1,6 +1,5 @@
 package bin;
 import java.awt.Desktop;
-import java.io.File;
 import java.net.URI;
 import java.net.URL;
 import java.security.ProtectionDomain;
@@ -12,7 +11,7 @@ public class StartServer {
 
 	private static final int DUMMY_PORT = 12345;
 	private static final String ROOT_CONTEXT_PATH = "/";
-	private static final String DATA_DIR = "data";
+
 	public static void main(String[] args) throws Exception {
 		int port =getPort(args);
 		String contextpath=getContextpath(args);
@@ -21,8 +20,7 @@ public class StartServer {
 		URL location = domain.getCodeSource().getLocation();
 		WebAppContext webapp = new WebAppContext();
 		webapp.setContextPath(contextpath);
-		webapp.setTempDirectory(new File(getData(args)));
-		webapp.setWar(location.toExternalForm());
+		webapp.setWar(location.toExternalForm()+"/webapp");
 		server.setHandler(webapp);
 		server.start();
 		
@@ -44,17 +42,6 @@ public class StartServer {
 		return contextpath;
 	}
 
-	private static String getData(String[] args) {
-		String  contextpath=DATA_DIR;
-		for (int i = 0; i < args.length; i++) {
-			if("-data".equals(args[i])){
-				contextpath=args[i+1];
-				break;
-			}
-		}
-		
-		return contextpath;
-	}
 	private static int getPort(String[] args) {
 		int port = DUMMY_PORT;
 		for (int i = 0; i < args.length; i++) {
